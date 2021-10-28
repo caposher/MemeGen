dfe8dc79e72982f0bebca8f2741d45ddb36cabe8
capos <caposher@gmail.com>
2021-10-28 11:38:02 +0300

    render pages, add more pictures andfix design

--- js/main.js
+++ js/main.js
@@ -1,6 +1,58 @@
 'use strict';
 
 function init() {
+  // renderGallery();
+  createCanvas();
+}
+
+function GetCanvas() {
+  const elCanvas = document.querySelector('.canvas');
+  return elCanvas;
+}
+
+function renderGallery() {
+  const elMain = document.querySelector('main');
+  const imgs = getImgs();
+  let strHTML = '<div class="img-grid main-layout grid">';
+
+  strHTML += imgs
+    .map((img) => {
+      return `<div class="img" onclick="onMemeSelected(${img.id})"  style="background-image: url('${img.url}');"></div>`;
+    })
+    .join('');
+  strHTML += '</div>';
+  elMain.innerHTML = strHTML;
+}
+
+function renderEditor() {
+  const elMain = document.querySelector('main');
+  let strHTML = `
+  <div class="editor-container main-layout flex">
+    <canvas class="canvas"></canvas>
+    <div class="panel-container flex">
+      <div class="panel grid">
+        <input
+          class="txt-input"
+          type="text"
+          placeholder="Text Line"
+          onfocus="this.value=''"
+          oninput="onText(this.value)"
+        />
+        <div class="move-up mouse-react" onclick="onMoveUp()"></div>
+        <div class="move-down mouse-react" onclick="onMoveDown()"></div>
+        <div class="switch-txt mouse-react" onclick="onChangeTextBox()"></div>
+        <div class="add-txt mouse-react" onclick="onAddText()"></div>
+        <div class="delete-txt mouse-react" onclick="onDeleteText()"></div>
+      </div>
+    </div>
+  </div>`;
+
+  elMain.innerHTML = strHTML;
+}
+
+function onMemeSelected(imgId) {
+  setEditorImg(imgId);
+  renderEditor();
   createCanvas();
 }
 
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -3,9 +3,30 @@ const MOVEMENT_STEP = 10;
 const RECT_PADDING = 10; //TODO: need to be dynamic
 let gElCanvas;
 let gCanvas;
+let gTxtAlignment = 'center';
 
 // var gKeywords = { happy: 12, 'funny puk': 1 };
-var gImgs = [{ id: 1, url: 'img/meme-imgs/6.jpg', keywords: ['happy'] }];
+var gImgs = [
+  { id: 1, url: 'img/meme-imgs/1.jpg', keywords: [] },
+  { id: 2, url: 'img/meme-imgs/2.jpg', keywords: [] },
+  { id: 3, url: 'img/meme-imgs/3.jpg', keywords: [] },
+  { id: 4, url: 'img/meme-imgs/4.jpg', keywords: [] },
+  { id: 5, url: 'img/meme-imgs/5.jpg', keywords: [] },
+  { id: 6, url: 'img/meme-imgs/6.jpg', keywords: [] },
+  { id: 7, url: 'img/meme-imgs/7.jpg', keywords: [] },
+  { id: 8, url: 'img/meme-imgs/8.jpg', keywords: [] },
+  { id: 9, url: 'img/meme-imgs/9.jpg', keywords: [] },
+  { id: 10, url: 'img/meme-imgs/10.jpg', keywords: [] },
+  { id: 11, url: 'img/meme-imgs/11.jpg', keywords: [] },
+  { id: 12, url: 'img/meme-imgs/12.jpg', keywords: [] },
+  { id: 13, url: 'img/meme-imgs/13.jpg', keywords: [] },
+  { id: 14, url: 'img/meme-imgs/14.jpg', keywords: [] },
+  { id: 15, url: 'img/meme-imgs/15.jpg', keywords: [] },
+  { id: 16, url: 'img/meme-imgs/16.jpg', keywords: [] },
+  { id: 17, url: 'img/meme-imgs/17.jpg', keywords: [] },
+  { id: 18, url: 'img/meme-imgs/18.jpg', keywords: [] },
+];
+
 var gMeme = {
   selectedImgId: 1,
   selectedLineIdx: 0,
@@ -14,13 +35,13 @@ var gMeme = {
 
 function createCanvas() {
   // change canvas size
-  gElCanvas = document.querySelector('.canvas');
-  gElCanvas.width = window.innerWidth / 2;
-  gElCanvas.height = (500 * gElCanvas.width) / 500; //TODO: do dynamic calc
+  gElCanvas = GetCanvas();
+  gElCanvas.width = 550;
+  gElCanvas.height = 550; //TODO: do dynamic calc
 
   //add image
   gCanvas = gElCanvas.getContext('2d');
-  renderCanvas(getCurrImg());
+  renderCanvas();
 }
 
 function renderCanvas() {
@@ -40,9 +61,15 @@ function renderText() {
       gCanvas.lineWidth = 2;
       gCanvas.strokeStyle = 'black';
       gCanvas.fillStyle = 'white';
+      gCanvas.textAlign = gTxtAlignment;
       gCanvas.font = `${meme.size}px impact`;
-      gCanvas.fillText(meme.txt, meme.coordinate.sX + RECT_PADDING, meme.coordinate.sY + meme.size);
-      gCanvas.strokeText(meme.txt, meme.coordinate.sX + RECT_PADDING, meme.coordinate.sY + meme.size);
+
+      // debugger;
+      gCanvas.fillText(meme.txt, gElCanvas.width / 2, meme.coordinate.sY + meme.size);
+      gCanvas.strokeText(meme.txt, gElCanvas.width / 2, meme.coordinate.sY + meme.size);
+
+      // gCanvas.fillText(meme.txt, meme.coordinate.sX + RECT_PADDING, meme.coordinate.sY + meme.size);
+      // gCanvas.strokeText(meme.txt, meme.coordinate.sX + RECT_PADDING, meme.coordinate.sY + meme.size);
     });
   }
 }
@@ -51,28 +78,24 @@ function renderSelected() {
   const selected = getSelected();
   if (selected) {
     selected.coordinate.eY = selected.size + RECT_PADDING;
-    // const rectHeight = selected.size + RECT_PADDING;
-    let rectLength = 0;
 
     //if empty text, rectangle size and location will change
     if (!selected.txt) {
-      selected.coordinate.eX = gElCanvas.width;
-      // rectLength = gElCanvas.width;
+      selected.coordinate.eX = gElCanvas.width - 2 * RECT_PADDING;
 
       //set the line in defferent location depending on the order
-      if (gMeme.selectedLineIdx === 1) {
-        selected.coordinate.sY = gElCanvas.height - selected.size;
-      } else if (gMeme.selectedLineIdx > 1) {
-        selected.coordinate.sY = gElCanvas.height / 2 - selected.size;
-      }
+      selected.coordinate.sY = _getYAxis();
+      selected.coordinate.sX = RECT_PADDING;
     } else {
-      selected.coordinate.eX = gCanvas.measureText(selected.txt).width + 2 * RECT_PADDING;
-      // rectLength = gCanvas.measureText(selected.txt).width + 2 * RECT_PADDING;
+      let { xStart, xEnd } = _getXAxis();
+      selected.coordinate.sX = xStart;
+      selected.coordinate.eX = xEnd;
+      // selected.coordinate.sX = gCanvas.measureText(selected.txt).width - 2 * RECT_PADDING;
+      // selected.coordinate.eX = gCanvas.measureText(selected.txt).width + 2 * RECT_PADDING;
     }
 
     gCanvas.beginPath();
     gCanvas.rect(selected.coordinate.sX, selected.coordinate.sY, selected.coordinate.eX, selected.coordinate.eY);
-    // gCanvas.rect(selected.coordinate.sX, selected.coordinate.sY, rectLength, rectHeight);
     gCanvas.strokeStyle = 'white';
     gCanvas.stroke();
   }
@@ -114,8 +137,17 @@ function changeTextBox() {
   renderCanvas();
 }
 
+function getImgs() {
+  return gImgs;
+}
+
+function setEditorImg(id) {
+  gMeme.selectedImgId = id;
+}
+
 function getCurrImg() {
-  return gImgs.find((img) => (img.id = gMeme.selectedImgId)).url;
+  // debugger;
+  return gImgs.find((img) => img.id === gMeme.selectedImgId).url;
 }
 
 function getSelected() {
@@ -133,7 +165,7 @@ function setText(txt) {
 function _createMeme() {
   return {
     txt: '',
-    size: 40,
+    size: 50,
     align: 'left',
     color: 'red',
     coordinate: {
@@ -143,4 +175,38 @@ function _createMeme() {
       eY: 0,
     },
   };
+}
+
+function _getYAxis() {
+  let YAxis;
+  switch (gMeme.selectedLineIdx) {
+    case 0:
+      YAxis = RECT_PADDING;
+      break;
+    case 1:
+      YAxis = gElCanvas.height - getSelected().size - 2 * RECT_PADDING;
+      break;
+    default:
+      YAxis = gElCanvas.height / 2 - getSelected().size;
+      break;
+  }
+  return YAxis;
+}
+
+function _getXAxis() {
+  let selected = getSelected();
+  let xStart;
+  let xEnd;
+  switch (gTxtAlignment) {
+    case 'center':
+      xStart = gElCanvas.width / 2 - gCanvas.measureText(selected.txt).width / 2 - RECT_PADDING;
+      xEnd = gCanvas.measureText(selected.txt).width + 2 * RECT_PADDING;
+      // debugger;
+      break;
+    case 'left':
+      break;
+    case 'right':
+      break;
+  }
+  return { xStart, xEnd };
 }
